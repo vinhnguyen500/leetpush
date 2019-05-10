8473aa1fefc541699377f2a67f931c85e61df35b
vinh <vinhnguyen500@hotmail.com>
2019-05-09 22:23:47 -0700

    Github Login Form
    Add login page for users to login to their github account.
    
    Grab credentials from form without actually submitting
    
    Need to use git-hub api to actually communicate.

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -7,6 +7,19 @@ var setColor = (color) => {
   document.body.style.backgroundColor = color;
 };
 
+var git_img = ext.extension.getURL('images/GitHub-Mark.png')
+document.getElementById('github-mark').src = git_img;
+document.getElementById('git-login').onsubmit = function() {
+  var kvpairs = [];
+  var form = document.getElementById('git-login')
+  for ( var i = 0; i < form.elements.length; i++ ) {
+    var e = form.elements[i];
+    kvpairs.push(encodeURIComponent(e.name) + "=" + encodeURIComponent(e.value));
+  }
+  
+  var queryString = kvpairs.join("&");
+  return false;
+}
 storage.get('color', function(resp) {
   var color = resp.color;
   var option;
